1de32ce56a05e2f41735798909b2b19535db37ef
Underchina <Underchina@DESKTOP-OMLRHT9>
2022-06-11 16:27:08 +0800

    Добавьте файлы проекта.

---
+++ QWERTY s 1 ex 3/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QWERTY_s_1_ex_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        QWERTYEntities1 context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new QWERTYEntities1();
+        }
+
+        int attempt = 3;
+        private void EnterClick(object sender, RoutedEventArgs e)
+        {
+            attempt--;
+
+            if (attempt == 0)
+            {
+
+                e.Source = passwordButton.Visibility = Visibility.Visible;
+                e.Source = enter.Visibility = Visibility.Hidden;
+            }
+            try
+            {
+                int tubNum = Convert.ToInt32(login.Text);
+                string pass = password.Text;
+
+                Worker worker = context.Worker.ToList().Find(x => x.tabNum == tubNum);
+
+                if (worker == null)
+                {
+                    MessageBox.Show("Неправильно введен логин, осталось попыток:" + attempt, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    if (worker.password.Equals(pass))
+                    {
+                        password.Background = Brushes.Green;
+                        MessageBox.Show("Успешная авторизация!", "Успешно!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        password.Background = Brushes.Red;
+                        MessageBox.Show("Неправильно введен пароль, осталось попыток:" + attempt, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+
+            }
+            catch (FormatException)
+            {
+
+                if (attempt == 0)
+                {
+                    MessageBox.Show("Неправильно введены данные, у вас не осталось попыток!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                if (attempt > 0)
+                {
+
+                    MessageBox.Show("Неправильно введены данные, осталось попыток:" + attempt, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.ShowDialog();
+        }
+    }
+}
---
+++ QWERTY s 1 ex 3/Window1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QWERTY_s_1_ex_3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        QWERTYEntities1 context;
+        public Window1()
+        {
+            InitializeComponent();
+            context = new QWERTYEntities1();
+        }
+
+        private void EnterClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int tubNum1 = Convert.ToInt32(tubNum.Text);
+                string fio1 = fio.Text;
+
+                int pos = Convert.ToInt32(positions.Text);
+                DateTime date1 = Convert.ToDateTime(date.Text);
+                Worker worker = context.Worker.ToList().Find(x => x.tabNum == tubNum1);
+                if (worker.tabNum.Equals(tubNum1) && worker.FIO.Equals(fio1) && worker.position.Equals(pos) && worker.dateEmp.Equals(date1))
+                {
+                    MessageBox.Show("Ваш пароль:" + Convert.ToString(worker.password), "Успешно!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Проверьте правильность написания данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+
+            catch
+            {
+                MessageBox.Show("Проверьте правильность написания данных!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+
+
+
+        }
+
+    }
+    
+}
